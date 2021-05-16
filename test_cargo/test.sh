47ee76f9c8ed2be862c850003c745a6c006c66af
milochen0418 <milo_chen@mail2000.com.tw>
2021-05-17 01:36:09 +0800

    Add comment

--- test_cargo/test.sh
+++ test_cargo/test.sh
@@ -3,9 +3,15 @@ cargo new hello_cargo
 cd hello_cargo
 echo "tree of hello_cargo before build"
 tree 
+# Cargo.toml file for Cargo  maybe like package.json file for npm 
+
 cargo build  
 echo "tree of hello_cargo after build"
 tree 
 cargo run 
 cd -
+
+# ./hello_cargo/target/debug/hello_cargo can be executed 
+# ./hello_cargo/Cargo.lock is generated according to Cargo.toml
 rm -rf hello_cargo
+
