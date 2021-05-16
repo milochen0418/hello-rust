a302d281037576913f6f78af3e1b2e50dcf16afb
milochen0418 <milo_chen@mail2000.com.tw>
2021-05-17 01:13:16 +0800

    test rustc

---
+++ test_rustc/hello.rs
@@ -0,0 +1,10 @@
+//Do the command 
+// $ rustc hello.rs && ./hello && rm hello
+//compile hello.rs to hello execution, execute it and then delete it.  
+fn main() {
+    // Print text to the console
+    println!("Hello World!");
+}
+
+
+
